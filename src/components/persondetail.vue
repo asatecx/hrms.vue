db1ba9d574af33ded8dd12dd69442bcc50abca83
spiderxman <3130569@qq.com>
2020-05-07 09:40:39 +0900

    no message

--- src/components/persondetail.vue
+++ src/components/persondetail.vue
@@ -67,7 +67,7 @@ export default {
     },
     interview(){
       // this.$router.push("/interview/makeinterview");
-      this.$router.push({name: "makeinterview", params: {personId : this.personDetail.id}});
+      this.$router.push({name: "makeinterview", params: {personId : this.personDetail.PERSON_ID}});
     },
   },
   created() {
